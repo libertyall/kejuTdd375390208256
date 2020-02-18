9a21b278baa55948252f2f5c9762a752148acdeb
xuejian <WTITsh2019>
2020-02-18 14:42:08 +0800

    Initial commit

---
+++ src/main/java/com/jiker/keju/AppRunner.java
@@ -0,0 +1,15 @@
+package com.jiker.keju;
+
+public class AppRunner {
+
+    public static void main(String[] args) {
+        /*TODO
+          1. args[0]为resources下的测试数据文件名，例如传入的args[0]值为"testData.txt"。
+          2. 你写的程序将把testDataFile作为参数加载此文件并读取文件内的测试数据，并对每条测试数据计算结果。
+          3. 将所有计费结果拼接并使用\n分割，然后保存到receipt变量中。
+         */
+        String testDataFile = args[0];
+        String receipt = "";
+        System.out.println(receipt);
+    }
+}
